ebf66b4e2a0387f9e9ec9783b4e1109e9195864b
Kurt Johnson <fazbat@gmail.com>
2018-12-01 22:14:08 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reducer ,{enableBatching}from "./reducers";
+import  { createStore, applyMiddleware } from "./redux";
+import { Provider }from "react-redux";
+import  Thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const createStoreWithMiddleware = applyMiddleware(Thunk)(createStore);
+const store=createStoreWithMiddleware(enableBatching(reducer));
+
+ ReactDOM.render(   
+    <Provider	store ={store}>
+       <App />
+    </Provider	>,
+    document.getElementById('container')
+  )
